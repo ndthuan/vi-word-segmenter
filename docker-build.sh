f7c8c61d6defe458e2fafea37319876fda004397
Thuan Nguyen <me@ndthuan.com>
2020-04-05 11:13:52 +0700

    Repo cleanup

--- docker-build.sh
+++ docker-build.sh
@@ -7,10 +7,11 @@ SERVICE_DIR=$(cd $(dirname "$0") || exit; pwd)
 
 cd "$SERVICE_DIR" || exit
 
-SERVICE_NAME=$(mvn -Dexec.executable='echo' -Dexec.args='${project.artifactId}' --non-recursive exec:exec -q)
-SERVICE_VERSION=$(mvn -Dexec.executable='echo' -Dexec.args='${project.version}' --non-recursive exec:exec -q)
+SERVICE_NAME=$(./mvnw -Dexec.executable='echo' -Dexec.args='${project.artifactId}' --non-recursive exec:exec -q)
+SERVICE_VERSION=$(./mvnw -Dexec.executable='echo' -Dexec.args='${project.version}' --non-recursive exec:exec -q)
+DOCKER_IMAGE="ndthuan/$SERVICE_NAME:$SERVICE_VERSION"
 
-mvn -Dmaven.test.skip clean package spring-boot:repackage
+./mvnw -Dmaven.test.skip clean package spring-boot:repackage
 
 cat > Dockerfile.tmp <<EOF
 FROM openjdk:14-jdk-alpine
@@ -25,8 +26,8 @@ CMD java $JAVA_OPTS -jar ${SERVICE_NAME}-${SERVICE_VERSION}.jar
 
 EOF
 
-docker build -t ndthuan/"${SERVICE_NAME}":"${SERVICE_VERSION}" -f Dockerfile.tmp .
+docker build -t "$DOCKER_IMAGE" -f Dockerfile.tmp .
 
 if [ "$1" == "push" ]; then
-  docker push ndthuan/"${SERVICE_NAME}":"${SERVICE_VERSION}"
+  docker push "$DOCKER_IMAGE"
 fi
